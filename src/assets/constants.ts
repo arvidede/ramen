fb680d05cbe07f383927ed58700f3f23b7fb691d
Arvid Edenheim <arvidede@icloud.com>
2020-10-10 18:21:11 +0200

    removed dummy data

--- src/assets/constants.ts
+++ src/assets/constants.ts
@@ -1,56 +1,5 @@
-export const photos: PhotoType[] = [
-    {
-        title: 'Yum',
-        path: 'https://source.unsplash.com/random',
-        key: Math.random(),
-    },
-    { title: 'Yum', path: require('../img/ramen.jpg'), key: Math.random() },
-    {
-        title: 'Yum',
-        path: 'https://source.unsplash.com/random',
-        key: Math.random(),
-    },
-    { title: 'Yum', path: require('../img/ramen.jpg'), key: Math.random() },
-    {
-        title: 'Yum',
-        path: 'https://source.unsplash.com/random',
-        key: Math.random(),
-    },
-    { title: 'Yum', path: require('../img/ramen.jpg'), key: Math.random() },
-    {
-        title: 'Yum',
-        path: 'https://source.unsplash.com/random',
-        key: Math.random(),
-    },
-    { title: 'Yum', path: require('../img/ramen.jpg'), key: Math.random() },
-    {
-        title: 'Yum',
-        path: 'https://source.unsplash.com/random',
-        key: Math.random(),
-    },
-    { title: 'Yum', path: require('../img/ramen.jpg'), key: Math.random() },
-    {
-        title: 'Yum',
-        path: 'https://source.unsplash.com/random',
-        key: Math.random(),
-    },
-    { title: 'Yum', path: require('../img/ramen.jpg'), key: Math.random() },
-    {
-        title: 'Yum',
-        path: 'https://source.unsplash.com/random',
-        key: Math.random(),
-    },
-    { title: 'Yum', path: require('../img/ramen.jpg'), key: Math.random() },
-    {
-        title: 'Yum',
-        path: 'https://source.unsplash.com/random',
-        key: Math.random(),
-    },
-    { title: 'Yum', path: require('../img/ramen.jpg'), key: Math.random() },
-]
-
 export interface PhotoType {
-    title: string
-    path: string
-    key: number
+    title?: string
+    src: string
+    key?: number
 }
