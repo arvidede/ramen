204fbb8497da5a03c369ba01d9a6456a00778415
Arvid Edenheim <arvidede@icloud.com>
2020-10-10 18:23:05 +0200

    fade-in onload image

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,18 +1,24 @@
-import React, { useState } from 'react'
-import { Spinner } from '../assets'
+import React, { useState, useEffect } from 'react'
+import { PhotoType } from '../assets/constants'
+import clsx from 'clsx'
+import '../styles/Image.scss'
 
-type ImageProps = {
-    src: string
+interface ImgProps extends PhotoType {
     className: string
 }
 
-export const Image: React.FC<ImageProps> = ({ src, className }) => {
-    const [imageLoaded, setImageLoaded] = useState(false)
+export const Img: React.FC<ImgProps> = ({ src, className }) => {
+    const [loadedSrc, setLoadedSrc] = useState('')
 
-    return (
-        <>
-            <img src={src} alt="" className={className} onLoad={() => setImageLoaded(true)} />
-            {!imageLoaded && <Spinner />}
-        </>
-    )
+    useEffect(() => {
+        const image = new Image()
+        image.onload = () => {
+            setLoadedSrc(src)
+        }
+        image.src = src
+    }, [src])
+
+    const name = clsx(className, 'img', loadedSrc !== '' && 'loaded')
+
+    return loadedSrc === '' ? <img className={name} /> : <img src={loadedSrc} alt="" className={name} />
 }
