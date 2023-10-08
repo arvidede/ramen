2b74c6506f98d4a8252356f13f6121d1c60f2314
Arvid Edenheim <arvidede@icloud.com>
2023-10-08 14:14:02 +0200

    Updates

---
+++ precache-manifest.f9b7285144e9b3eadcfa5b3987aaaefb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a209a2da88f86ef997dcfd44ca3df47",
+    "url": "/ramen/index.html"
+  },
+  {
+    "revision": "82be363414c1f3e785f6",
+    "url": "/ramen/static/css/main.b4ba6098.chunk.css"
+  },
+  {
+    "revision": "7029af3fd1e90b02f83c",
+    "url": "/ramen/static/js/2.d081d6d6.chunk.js"
+  },
+  {
+    "revision": "b01cf20ee08477db845d02c3b279da13",
+    "url": "/ramen/static/js/2.d081d6d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82be363414c1f3e785f6",
+    "url": "/ramen/static/js/main.ac0d44a7.chunk.js"
+  },
+  {
+    "revision": "00b806fd94f36e7663a1",
+    "url": "/ramen/static/js/runtime-main.2647f9f1.js"
+  }
+]);
